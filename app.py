41a74c07d5a80ae466e3c60cdc28c6afd96fa186
hayato131313 <hayato20010621@gmail.com>
2025-06-26 21:01:31 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,114 @@
+from flask import Flask,render_template,url_for,redirect,session,flash,request
+from flask_wtf import FlaskForm
+from wtforms import ValidationError, StringField,PasswordField,SubmitField
+from wtforms.validators import Email,DataRequired,EqualTo
+
+import os 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+from pytz import timezone
+
+app=Flask(__name__)
+
+app.config['SECRET_KEY']='mysecretkey'
+
+basedir=os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+Migrate(app,db)
+
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+class User(db.Model):
+    __tablename__='users'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String(64),unique=True,index=True)
+    username=db.Column(db.String(64),unique=True,index=True)
+    password_hash=db.Column(db.String(128))
+    administrator=db.Column(db.String(1))
+    post=db.relationship('BlogPost',backref='author',lazy='dynamic')
+
+    def __init__(self,email,username,password_hash,administrator):
+        self.email=email
+        self.username=username
+        self.password_hash=password_hash
+        self.administrator=administrator
+
+    def __repr__(self):
+        return f'UserName:{self.username}'
+    
+class BlogPost(db.Model):
+    __tablename__='blog_post'
+    id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+    date=db.Column(db.DateTime,default=datetime.now(timezone('Asia/Tokyo')))
+    title=db.Column(db.String(140))
+    text=db.Column(db.Text)
+    summary=db.Column(db.String(140))
+    featured_image=db.Column(db.String(140))
+
+    def __init__(self,title,text,featured_image,user_id,summary,):
+        self.text=text
+        self.summary=summary
+        self.featured_image=featured_image
+        self.user_id=user_id
+        self.title=title
+    
+    def __repr__(self):
+        return f'PostID:{self.id},Title:{self.title},Author:{self.author}\n'
+
+class RegistrationForm(FlaskForm):
+    email=StringField('メールアドレス',validators=[Email(message='正しいメールアドレスを入力してください'),DataRequired()])
+    username=StringField('ユーザー名',validators=[DataRequired()])
+    password=PasswordField('パスワード',validators=[DataRequired(),EqualTo('pass_confirm',message='パスワードが一致しません')])
+    pass_confirm=PasswordField('パスワード（確認）',validators=[DataRequired()])
+    submit=SubmitField('登録')
+
+    def validate_username(self,field) :
+        if User.query.filter_by(username=field.data).first:
+            raise ValidationError('入力されたユーザー名は既に使用されています。')
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first:
+            raise ValidationError('入力されたメールアドレスは既に登録されています。')
+
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        # session['email']=form.email.data
+        # session['username'] = form.username.data
+        # session['password'] = form.password.data
+        user=User(email=form.email.data,username=form.username.data,password_hash=form.password.data,administrator='0')
+        db.session.add(user)
+        db.session.commit()
+        flash('ユーザーが登録されました')
+        return redirect(url_for('user_maintenance'))
+    return render_template('register.html',form=form)
+
+@app.route('/user_maintenance')
+def user_maintenance():
+    page=request.args.get('page',1,type=int)
+    users=User.query.order_by(User.id).paginate(page=page,per_page=10)
+    return render_template('user_maintenance.html',users=users)
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+
+    
+
